9c108fa3deb2794b2bd5745f82d6c22e880be95f
Kumar Mashalkar <kumar.mashalkar@kumar.mashalkar>
2026-02-25 09:48:38 +0530

    t2

---
+++ gcp.tf
@@ -0,0 +1,80 @@
+############################################################
+# Provider configuration (edit for your environment)
+############################################################
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.0"
+    }
+  }
+}
+
+
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
+
+variable "project_id" {
+  description = "GCP project ID"
+  type        = string
+}
+
+
+variable "region" {
+  description = "GCP region"
+  type        = string
+  default     = "us-central1"
+}
+
+############################################################
+# 1) Public GCS bucket (likely flagged as publicly readable)
+############################################################
+
+resource "google_storage_bucket" "public_bucket" {
+  name     = "${var.project_id}-public-demo-bucket"
+  location = var.region
+
+  # Disable uniform bucket-level access (bad practice)
+  uniform_bucket_level_access = false
+
+  # Explicitly allow public access (bad)
+  public_access_prevention = "unspecified"
+}
+
+# Grant allUsers read access to objects in the bucket (bad)
+resource "google_storage_bucket_iam_member" "public_read" {
+  bucket = google_storage_bucket.public_bucket.name
+  role   = "roles/storage.objectViewer"
+  member = "allUsers"
+}
+
+############################################################
+# 2) Overly permissive firewall rule (0.0.0.0/0, all TCP ports)
+############################################################
+
+resource "google_compute_network" "demo_vpc" {
+  name                    = "demo-vpc"
+  auto_create_subnetworks = true
+}
+
+resource "google_compute_firewall" "allow_all_ingress" {
+  name    = "allow-all-ingress"
+  network = google_compute_network.demo_vpc.name
+
+  direction = "INGRESS"
+  priority  = 1000
+
+  # Allow *any* source (0.0.0.0/0) – bad
+  source_ranges = ["0.0.0.0/0"]
+
+  # Allow all TCP ports – bad
+  allow {
+    protocol = "tcp"
+    ports    = ["0-65535"]
+  }
+
+  # Tag not required; rule applies broadly
+}
